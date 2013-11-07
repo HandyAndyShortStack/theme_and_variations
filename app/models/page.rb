2818a7d3a742d1b30de4a4f67c1d2fa635667565
Andy Short <andynshort@gmail.com>
2013-11-07 08:46:46 -0800

    removed content areas

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,6 @@
 class Page < ActiveRecord::Base
   attr_accessible :template_id, :options, :site_id, :title, :url
   serialize :options, Hash
-  has_many :content_areas
   belongs_to :template
   belongs_to :site
 
