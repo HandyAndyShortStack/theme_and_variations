de16ee4af1bd7d16f6e59492bd11b02152861d20
Andy Short <andynshort@gmail.com>
2013-11-07 13:15:00 -0800

    made blocks render things from erb partials

--- app/models/blocks/block.rb
+++ app/models/blocks/block.rb
@@ -2,4 +2,14 @@ class Block < ActiveRecord::Base
   attr_accessible :options, :sandbox_id
   belongs_to :sandbox
   serialize :options, Hash
+
+  def render
+    ActionView::Base.new(Rails.configuration.paths["app/views"]).render({
+      partial: 'blocks/layout',
+      format: :txt, 
+      locals:  { 
+        block: self
+      }
+    })
+  end
 end
