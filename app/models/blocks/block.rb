e3d109873458b94f74565525e2401f2f44c93683
Andy Short <andynshort@gmail.com>
2013-11-07 13:56:41 -0800

    implemented block subclasses and rendering therof

--- app/models/blocks/block.rb
+++ app/models/blocks/block.rb
@@ -3,6 +3,18 @@ class Block < ActiveRecord::Base
   belongs_to :sandbox
   serialize :options, Hash
 
+  def method_missing method_name
+    if response = options[method_name]
+      response
+    else
+      super
+    end
+  end
+
+  def respond_to? method_name, include_all=false
+    super || !!options[method_name]
+  end
+
   def render
     ActionView::Base.new(Rails.configuration.paths["app/views"]).render({
       partial: 'blocks/layout',
@@ -11,5 +23,9 @@ class Block < ActiveRecord::Base
         block: self
       }
     })
+  end
+
+  def block_type
+    self.class.to_s.underscore
   end
 end
