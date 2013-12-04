d7ee670f120a42fa43b57b1e256d1e06b7fce643
Andy Short <andynshort@gmail.com>
2013-12-04 11:10:30 -0800

    fixed id incrementation bug

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -3,8 +3,8 @@ module ThemesHelper
     testing_theme_names = Dir.entries("#{Rails.root.to_s}/themes") - [".", ".."]
     TestingTheme.all.each { |theme| theme.destroy unless testing_theme_names.include? theme.name }
     testing_theme_names.each do |name|
-      theme = TestingTheme.find_by_name(name) || TestingTheme.create(name: name)
-      site = theme.sites.find_by_subdomain(name) || theme.sites.create
+      theme = TestingTheme.find_or_create_by_name(name)
+      site = theme.sites.find_or_create_by_subdomain(name)
       site.update_attributes({
         subdomain:     name,
         theme_id:      theme.id,
@@ -18,10 +18,12 @@ module ThemesHelper
           description: "sample description"
         }
       })
-      testing_template_names = Dir.entries("#{Rails.root.to_s}/themes/#{name}/templates") - [".", ".."]
+      testing_template_names = Dir.entries("#{Rails.root.to_s}/themes/#{name}/templates").-([".", ".."]).map do |file_name|
+        file_name.sub ".liquid", ""
+      end
       site.pages.each { |page| page.destroy unless testing_template_names.include? page.url }
       theme.templates.each do |template|
-        page = site.pages.find_by_url(template.name) || site.pages.create
+        page = site.pages.find_or_create_by_url(template.name)
         page.update_attributes({
           template_id: template.id,
           url:         template.name,
