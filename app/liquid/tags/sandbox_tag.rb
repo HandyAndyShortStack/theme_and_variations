8c0d304add064e143e953daf73df197413b9b079
Aaron Trostle <aaron.trostle@gmail.com>
2013-12-04 10:45:24 -0800

    make sure sandboxes are named

--- app/liquid/tags/sandbox_tag.rb
+++ app/liquid/tags/sandbox_tag.rb
@@ -1,12 +1,12 @@
 class SandboxTag < Liquid::Tag
   def initialize(tag_name, sandbox_name, tokens)
      super
-     @sandbox_name = sandbox_name
+     @sandbox_name = sandbox_name.strip
   end
 
   def render(context)
     page = context.environments[0]["page"].instance_eval { @page }
-    sandbox = page.sandboxes.find_by_name(@sandbox_name) || page.sandboxes.create
+    sandbox = page.sandboxes.find_or_create_by_name(@sandbox_name)
     sandbox.render
   end
 end
